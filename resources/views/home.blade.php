38906bdfd426f89c2c79940e3d957d00b33a2416
Axcellent <axcellentwalukow@gmail.com>
2024-04-08 15:09:39 +0700

    new file:   resources/views/commonTemplate.blade.php 	modified:   resources/views/home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,26 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <title>Home &mdash; Voice of Soul Choir</title>
-    
-    <link href="https://fonts.googleapis.com/css?family=Crimson+Text:400,400i,600|Montserrat:200,300,400, 600, 700" rel="stylesheet">
-
-    <link rel="stylesheet" href="assets/css/bootstrap/bootstrap.css">
-    <link rel="stylesheet" href="assets/fonts/ionicons/css/ionicons.min.css">
-    <link rel="stylesheet" href="assets/css/magnific-popup.css">
-
-    <link rel="stylesheet" href="assets/fonts/fontawesome/css/font-awesome.min.css">
-    
-    
-    <link rel="stylesheet" href="assets/css/slick.css">
-
-    <link rel="stylesheet" href="assets/css/helpers.css">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="assets/css/restaurant.css">
-  </head>
+@extends('commonTemplate')
+@section('content')
   <body data-spy="scroll" data-target="#pb-navbar" data-offset="200">
     
 
@@ -403,49 +382,4 @@
 </section>
 <!-- END section -->
 
-
-    <footer class="pb_footer bg-light" role="contentinfo">
-      <div class="container">
-        <div class="row text-center">
-          <div class="col">
-            <ul class="list-inline">
-              <li class="list-inline-item"><a href="#" class="p-2"><i class="fa fa-facebook"></i></a></li>
-              <li class="list-inline-item"><a href="#" class="p-2"><i class="fa fa-twitter"></i></a></li>
-              <li class="list-inline-item"><a href="#" class="p-2"><i class="fa fa-instagram"></i></a></li>
-              <li class="list-inline-item"><a href="#" class="p-2"><i class="fa fa-tripadvisor"></i></a></li>
-            </ul>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col text-center">
-            <p>&copy; 2024 <a href="">Voice of Soul Choir</a>. All Rights Reserved. Designed by <a href="">Axcellent Christian</a></p>
-          </div>
-        </div>
-      </div>
-    </footer>
-
-    <!-- loader -->
-    <div id="pb_loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#FDA04F"/></svg></div>
-    
-
-    <script src="assets/js/jquery.min.js"></script>
-    
-    <script src="assets/js/popper.min.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-    <script src="assets/js/slick.min.js"></script>
-    <script src="assets/js/jquery.mb.YTPlayer.min.js"></script>
-
-    <script src="assets/js/jquery.waypoints.min.js"></script>
-    <script src="assets/js/jquery.easing.1.3.js"></script>
-    
-    <script src="assets/js/jquery.magnific-popup.min.js"></script>
-    <script src="assets/js/magnific-popup-options.js"></script>
-  
-
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
-    <script src="assets/js/google-map.js"></script>
-
-    <script src="assets/js/main.js"></script>
-    
-  </body>
-</html>
+@endsection
