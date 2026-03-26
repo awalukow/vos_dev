d0a844f6b0d65585cd26f917055c1ca15da36daa
Axcellent <axcellentwalukow@gmail.com>
2026-03-26 15:59:57 +0700

    modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/system/migrate', function () {
+
+    if (request('key') !== 'migratenow') {
+        abort(403, 'Unauthorized');
+    }
+
+    Artisan::call('migrate', ['--force' => true]);
+    Artisan::call('optimize:clear');
+
+    return nl2br(Artisan::output());
+
+});
+
 Route::get('/', function () {
     return view('home');
 });
